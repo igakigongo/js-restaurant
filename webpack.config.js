05c086f7dda052d8f51e5a247a1c0fc60829cefe
Edward Iga Kigongo <igakigongo@gmail.com>
2020-01-29 09:32:55 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/* global __dirname, module, require */
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
